d092bb3c8bf6966ee9277ed25d114e61beeda073
Chrishantha Rajasekara <you@example.com>
2019-11-12 21:32:03 +0530

    get data from web api

--- Store_UI/Controllers/AlbumController.cs
+++ Store_UI/Controllers/AlbumController.cs
@@ -1,9 +1,14 @@
-﻿using Store_Domain.Service;
+﻿using Newtonsoft.Json;
+using Store_Domain;
+using Store_Domain.Service;
 using Store_UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace Store_UI.Controllers
@@ -11,20 +16,42 @@ namespace Store_UI.Controllers
     public class AlbumController : Controller
     {
         IStoreService _storeService = null;
+        private string BaseURI = WebConfigurationManager.AppSettings["BaseUri"];
+
         public AlbumController(IStoreService storeService)
         {
             _storeService = storeService;
         }
-        // GET: Album
-        public ViewResult AllAlbums()
+        //// GET: Album details from web api
+        public async Task<ViewResult> AllAlbums()
         {
-            var albums = _storeService.GetAllAlbums();
-            ProductListViewModel productListViewModel = new ProductListViewModel()
+            ProductListViewModel productListViewModel = new ProductListViewModel();
+            using (var client = new HttpClient())
             {
-                ShowingAlbums = albums
-            };
+                client.BaseAddress = new Uri(BaseURI);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage responseMessage = await client.GetAsync("album/albums");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var data = await responseMessage.Content.ReadAsStringAsync();
+                    productListViewModel.ShowingAlbums = JsonConvert.DeserializeObject<List<Album>>(data);
+                }
+            }
 
             return View(productListViewModel);
         }
+       
+        //// GET: Album details from database
+        //public ViewResult AllAlbums()
+        //{
+        //    var albums = _storeService.GetAllAlbums();
+        //    ProductListViewModel productListViewModel = new ProductListViewModel()
+        //    {
+        //        ShowingAlbums = albums
+        //    };
+
+        //    return View(productListViewModel);
+        //}
     }
 }
